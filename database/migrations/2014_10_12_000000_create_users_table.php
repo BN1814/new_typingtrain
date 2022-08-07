f8895dcd16e36a891a3d9ac4e9b74c6c895d686f
BN1814 <supina_p@silpakorn.edu>
2022-08-07 13:50:49 +0700

    Update Register

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('userid')->unique()->nullable();
+            $table->string('userid');
             $table->string('name');
             $table->string('email')->unique();
             $table->boolean('role')->nullable();
