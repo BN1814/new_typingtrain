61a8899d1451f46e2d5e0db3fc8ba66884b47e32
firstfirst907 <chinphram_a@silpakorn.edu>
2022-12-01 18:53:25 +0700

    edit HomeEx

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -135,10 +135,10 @@ class UserController extends Controller
                             ->groupBy('user_id', 'section_id', 'exercise_id')
                             ->get()->count();
         $count_exercises_fail = HistoryScore::select('exercise_id', 'user_id', 'section_id', \DB::raw('MAX(history_scores.score) as score'))
-                            ->groupBy('user_id', 'section_id', 'exercise_id')
                             ->where('user_id', $user_id)
                             ->where('section_id', $section->id)
-                            ->where('score', '<', '50')                            
+                            ->groupBy('user_id', 'section_id', 'exercise_id')   
+                            ->where('score', '<', '50')                           
                             ->get()->count();
         $total_scores = Exercise::get()->count()*100;
         $total_exercises = Exercise::get()->count();
@@ -152,7 +152,6 @@ class UserController extends Controller
             if($checkusersub == 0){
                 return redirect('user/enterclass');
             }else {
-                // return view('dashboards.users.homeEx', compact('section', 'user', 'historys',  'total_scores','checkuser','checkusersub'));
                 return view('dashboards.users.homeEx', compact('section', 'user', 'historys', 'count_exercises_pass' , 'count_exercises_fail' ,'count_exercises' , 'total_exercises' , 'total_scores','checkuser','checkusersub'));
             }      
         }
